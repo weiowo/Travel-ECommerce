08ca1b8b89612827dc553e0c2458b302089c23e8
weiwei <weiwei@ncfc.cc>
2025-02-21 15:48:26 +0800

    add products apis

--- app/seller/product-list/page.jsx
+++ app/seller/product-list/page.jsx
@@ -5,21 +5,38 @@ import Image from 'next/image';
 import { useAppContext } from '@/context/AppContext';
 import Footer from '@/components/seller/Footer';
 import Loading from '@/components/Loading';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const ProductList = () => {
-  const { router } = useAppContext();
+  const { router, getToken, user } = useAppContext();
 
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  console.log('products', products);
 
   const fetchSellerProduct = async () => {
-    setProducts(productsDummyData);
-    setLoading(false);
+    try {
+      const token = await getToken();
+      const { data } = await axios.get('/api/product/seller-list', {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (data.success) {
+        setProducts(data.products);
+        setLoading(false);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
-    fetchSellerProduct();
-  }, []);
+    if (user) {
+      fetchSellerProduct();
+    }
+  }, [user]);
 
   return (
     <div className="flex-1 min-h-screen flex flex-col justify-between">
